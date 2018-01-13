2b0c9ab7e7212d54f80c32fb2644b727f0234c47
Nick <1103137207@gm.kuas.edu.tw>
2018-01-13 13:06:23 +0800

    修正Bugs

--- app/src/main/java/com/example/no24519/projectchat/ChatActivity.java
+++ app/src/main/java/com/example/no24519/projectchat/ChatActivity.java
@@ -232,7 +232,7 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
-        /*mChatAddBtn.setOnClickListener(new View.OnClickListener() {
+        mChatAddBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -244,7 +244,7 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
-*/
+
 
         mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
